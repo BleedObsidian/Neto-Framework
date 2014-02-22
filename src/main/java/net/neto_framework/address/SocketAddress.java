aefa4fce835a9e0cf11111053ea27a0821300822
BleedObsidian <bleedobsidian@gmail.com>
2014-02-22 16:18:06 +0000

    Add SocketAddress
    Add an InetAddress wrapper to handle IP and port binding when creating sockets.

---
+++ src/main/java/net/neto_framework/address/SocketAddress.java
@@ -0,0 +1,122 @@
+/*
+    Neto-Framework, a lightweight, event driven network application framework.
+    Copyright (C) 2014  BleedObsidian (Jesse Prescott)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.neto_framework.address;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import net.neto_framework.address.exceptions.SocketAddressException;
+
+/**
+ * A socket address used to define what address to bind to when creating a new
+ * socket.
+ * 
+ * @author BleedObsidian (Jesse Prescott)
+ */
+public class SocketAddress {
+    private final InetAddress address;
+    private final int port;
+
+    /**
+     * New SocketAddress with localhost and given port.
+     * 
+     * @param port
+     *            Port number. (Max: 65535)
+     * @throws SocketAddressException
+     *             Failed to retrieve localhost.
+     */
+    public SocketAddress(int port) throws SocketAddressException {
+        try {
+            this.address = InetAddress.getLocalHost();
+
+            if (port <= 65535) {
+                this.port = port;
+            } else {
+                throw new SocketAddressException("Port number " + port
+                        + " out of range. (Max: 65535)");
+            }
+        } catch (UnknownHostException e) {
+            throw new SocketAddressException("Failed to resolve localhost.", e);
+        }
+    }
+
+    /**
+     * New SocketAddress with given host name and port.
+     * 
+     * @param host
+     *            Host name. (Can be an IP address or machine name)
+     * @param port
+     *            Port number. (Max: 65535)
+     * @throws SocketAddressException
+     */
+    public SocketAddress(String host, int port) throws SocketAddressException {
+        try {
+            this.address = InetAddress.getByName(host);
+
+            if (port <= 65535) {
+                this.port = port;
+            } else {
+                throw new SocketAddressException("Port number " + port
+                        + " out of range. (Max: 65535)");
+            }
+        } catch (UnknownHostException e) {
+            throw new SocketAddressException(
+                    "IP address for the host could not be found.", e);
+        }
+    }
+
+    /**
+     * New SocketAddress with given host name and port.
+     * 
+     * @param address
+     *            Raw IP address in network byte order .
+     * @param port
+     *            Port number. (Max: 65535)
+     * @throws SocketAddressException
+     */
+    public SocketAddress(byte[] address, int port)
+            throws SocketAddressException {
+        try {
+            this.address = InetAddress.getByAddress(address);
+
+            if (port <= 65535) {
+                this.port = port;
+            } else {
+                throw new SocketAddressException("Port number " + port
+                        + " out of range. (Max: 65535)");
+            }
+        } catch (UnknownHostException e) {
+            throw new SocketAddressException("IP address is too long.", e);
+        }
+    }
+
+    /**
+     * @return - InetAddress of SocketAddress.
+     */
+    public InetAddress getInetAddress() {
+        return this.address;
+    }
+
+    /**
+     * @return - Port number of SocketAddress.
+     */
+    public int getPort() {
+        return this.port;
+    }
+}
---
+++ src/main/java/net/neto_framework/address/exceptions/SocketAddressException.java
@@ -0,0 +1,50 @@
+/*
+    Neto-Framework, a lightweight, event driven network application framework.
+    Copyright (C) 2014  BleedObsidian (Jesse Prescott)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.neto_framework.address.exceptions;
+
+/**
+ * An exception thrown when failing to create a ServerAddress.
+ * 
+ * @author BleedObsidian (Jesse Prescott)
+ */
+public class SocketAddressException extends Exception {
+    private static final long serialVersionUID = -3147497186902377305L;
+
+    /**
+     * New ServerAddressException.
+     * 
+     * @param message
+     *            Reason for exception.
+     */
+    public SocketAddressException(String message) {
+        super(message);
+    }
+
+    /**
+     * New ServerAddressException with cause exception.
+     * 
+     * @param message
+     *            Reason for exception.
+     * @param exception
+     *            Cause exception.
+     */
+    public SocketAddressException(String message, Exception exception) {
+        super(message, exception);
+    }
+}
